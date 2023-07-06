d0977b94ec37bd8bd7574905d174a0610559b933
huiju0502 <yhj960502@naver.com>
2023-07-06 14:05:46 +0900

    로그인시 아이디저장 기능 추가(쿠키)

--- cashbook/src/main/java/cash/controller/LoginController.java
+++ cashbook/src/main/java/cash/controller/LoginController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,6 +22,16 @@ public class LoginController extends HttpServlet {
 		if(session.getAttribute("loginMember") != null) {
 			response.sendRedirect(request.getContextPath()+"/calendar");
 		}
+		
+		// 쿠키에 저장된 아이디가 있다면 request 속성에 저장
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null) {
+			for(Cookie c : cookies) {
+				if(c.getName().equals("loginId") == true) {
+					request.setAttribute("loginId", c.getValue());
+				}
+			}
+		}
 		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
 	}
 	@Override
@@ -33,16 +44,23 @@ public class LoginController extends HttpServlet {
 		MemberDao memberDao = new MemberDao();
 		Member loginMember = memberDao.selectMemberById(member);
 		
-		if(loginMember == null) { // null 로그인 실패
+		// 로그인 실패시
+		if(loginMember == null) {
 			System.out.println("로그인 실패");
 			response.sendRedirect(request.getContextPath() + "/login");
 			return;
 		} 
 		
-		// 로그인 성공시 : session 사용
+		// 로그인 성공시
+		// session에 저장
 		HttpSession session = request.getSession();
 		System.out.println("로그인 성공");
 		session.setAttribute("loginMember", loginMember);
+		// 쿠키에 저장
+		if(request.getParameter("idSave") != null) {
+			Cookie loginIdCookie =  new Cookie("loginId", loginMember.getMemberId());
+			response.addCookie(loginIdCookie);
+		}
 		response.sendRedirect(request.getContextPath()+"/calendar");
 		
 	}
--- cashbook/src/main/webapp/WEB-INF/view/login.jsp
+++ cashbook/src/main/webapp/WEB-INF/view/login.jsp
@@ -4,21 +4,22 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>login.jsp</title>
+<title>로그인</title>
 </head>
 <body>
 	<h1>로그인</h1>
 	<form action="${pageContext.request.contextPath}/login" method="post">
 		<table border="1">
 			<tr>
-				<td>memberId</td>
-				<td><input type="text" name="memberId"></td>
+				<td>아이디</td>
+				<td><input type="text" name="memberId" value="${loginId}"></td>
 			</tr>
 			<tr>
-				<td>memberPw</td>
+				<td>비밀번호</td>
 				<td><input type="password" name="memberPw"></td>
 			</tr>
 		</table>
+		<input type="checkbox" name="idSave" value="y">ID저장
 		<button type="submit">로그인</button>
 		<a href="${pageContext.request.contextPath}/addMember">회원가입</a>
 	</form>
